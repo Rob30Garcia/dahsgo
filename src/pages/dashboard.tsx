5cc89d04c0208509515e18c6891372ed7465e4fe
Robert Garcia <rgs2@icomp.ufam.edu.br>
2022-03-23 16:27:39 -0400

    feat: add charts

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,58 @@
-import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
 import { Header } from "../Components/Header";
 import { Sidebar } from "../Components/Sidebar";
+import { ApexOptions } from 'apexcharts';
+import dynamic from "next/dynamic";
+
+const Chart = dynamic(() => import('react-apexcharts'), {
+  ssr: false
+});
+
+const options: ApexOptions = {
+  chart: {
+    toolbar: {
+      show: false,
+    },
+    zoom: {
+      enabled: false,
+    },
+    foreColor: theme.colors.gray[500]
+  },
+  grid: {
+    show: false,
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  tooltip: {
+    enabled: false,
+  },
+  xaxis: {
+    type: 'datetime',
+    axisBorder: {
+      color: theme.colors.gray[600]
+    },
+    axisTicks: {
+      color: theme.colors.gray[600]
+    },
+    categories: [
+      '2021-03-18T00:00:00.000z',
+      '2021-03-19T00:00:00.000z',
+      '2021-03-20T00:00:00.000z',
+      '2021-03-21T00:00:00.000z',
+      '2021-03-22T00:00:00.000z',
+      '2021-03-23T00:00:00.000z',
+      '2021-03-24T00:00:00.000z',
+    ]
+  }
+}
+
+const series = [
+  { 
+    name: 'serie1', 
+    data:  [31, 120, 10, 28, 61, 18, 109]
+  }
+];
 
 export default function Dashboard() {
   return (
@@ -23,6 +75,8 @@ export default function Dashboard() {
             >
               Inscritos da semana
             </Text>
+
+            <Chart options={options} series={series} type="area" height={160}/>
           </Box>
           <Box
             p="8"
@@ -36,6 +90,8 @@ export default function Dashboard() {
             >
               Taxa de abertura
             </Text>
+
+            <Chart options={options} series={series} type="area" height={160}/>
           </Box>
         </SimpleGrid>
       </Flex>
