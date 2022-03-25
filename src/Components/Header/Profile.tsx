df95490266949db69262dd6fddd371f54825a79a
Robert Garcia <rgs2@icomp.ufam.edu.br>
2022-03-25 18:31:51 -0400

    chore: add header responsive

--- src/Components/Header/Profile.tsx
+++ src/Components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mx="4" textAlign="right">
-        <Text>Robert Garcia</Text>
-        <Text color="gray.300" fontSize="small">
-          robert.garcia@gmail.com
-        </Text>
-      </Box>
+      {showProfileData &&
+        <Box mx="4" textAlign="right">
+          <Text>Robert Garcia</Text>
+          <Text color="gray.300" fontSize="small">
+            robert.garcia@gmail.com
+          </Text>
+        </Box>
+      }
 
       <Avatar size="md" name="Robert Garcia" src="http://github.com/Rob30Garcia.png"/>
     </Flex>
