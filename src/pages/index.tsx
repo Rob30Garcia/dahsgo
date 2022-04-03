a8c27154e95956c828dbdcdfa347afe1d684b9d9
Robert Garcia <rgs2@icomp.ufam.edu.br>
2022-04-03 12:49:56 -0400

    feat: add authentication forms

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
+import { useForm } from 'react-hook-form';
 import { Button, Flex, Stack } from "@chakra-ui/react";
 import { Input } from "../Components/Form/Input";
 
+
 export default function Home() {
+  const { register, handleSubmit, formState } = useForm();
+
+  async function handleSignIn(values) {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    console.log(values);
+  }
+
   return (
     <Flex
       w="100vw"
@@ -17,18 +27,21 @@ export default function Home() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
           <Input 
             name="email"
             type="email"
             label="E-mail"
+            {...register('email')}
           />
 
           <Input 
             name="password"
             type="password"
             label="Senha"
+            {...register('password')}
           />
         </Stack>
 
@@ -36,6 +49,7 @@ export default function Home() {
           type="submit"
           mt="6"
           colorScheme="pink"
+          isLoading={formState.isSubmitting}
         >
           Entrar
         </Button>
